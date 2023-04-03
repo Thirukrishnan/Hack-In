a05c289bc8b5db0bb19b1bde07066b28b10e0eee
Thirukrishnan <63901950+Thirukrishnan@users.noreply.github.com>
2023-04-03 18:23:30 +0530

    Add files via upload

---
+++ rev/chall3.py
@@ -0,0 +1,41 @@
+txt=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','_','0','1','3','}','2','4','5','6','7','8','9']
+
+part1="thiran{K3yg3n_Cr4ck1ng_1s"
+
+user=input("Enter keygen: ")
+
+
+if user[0]==txt[0x1a] and user[1]==txt[11] and user[2]==txt[0x1c] and user[3]==txt[10] and user[4]==txt[0x1d] and user[5]==txt[26]:
+    if user[6]==txt[0xb] and user[7]==txt[27] and user[8]==txt[0x2] and user[9]==txt[10] and user[10]==txt[0xf] and user[11]==txt[28]:
+        if user[12]==txt[0x2] and user[13]==txt[10] and user[14]==txt[0x1c] and user[15]==txt[13] and user[16]==txt[0x6] and user[17]==txt[26]:
+            if user[18]==txt[0x1b] and user[19]==txt[5] and user[20]==txt[0x1a] and user[21]==txt[18] and user[22]==txt[0x1b] and user[23]==txt[5]:
+                if user[24]==txt[0x13] and user[25]==txt[22] and user[26]==txt[0x1d] and user[27]==txt[17] and user[28]==txt[0x1d] and user[29]==txt[18] and user[30]==txt[0x1e]:
+                    print("Success")  
+                else:
+                    print("Failed")
+            else:
+                print("Failed")
+        else:
+            print("Failed")
+    else:
+        print("Failed")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
